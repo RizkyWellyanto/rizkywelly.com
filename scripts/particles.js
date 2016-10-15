b97ea9fe5e6a1fcffc6082e22491f32a02aa7f47
Rizky <wellyanto.rizky@gmail.com>
2016-10-15 18:35:30 -0500

    remaking the whole web. removing angular. starting a new life

--- scripts/particles.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by welly on 1/3/2016.
- */
-
-particlesJS.load('particles-js', '../json/misc/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-
--- server.js
+++ server.js
@@ -2,17 +2,7 @@
  * Created by MuhammadRizky on 8/3/14.
  */
 
-// Simple node server - CS22A
-
-// The following function will be called whenever
-
-// the server receives a request.
-
 function servePage(request, response) {
-
-    // MIME types supported by this server: html, txt, js, appcache, css and json
-
-    // Create an object supportedTypes that maps file extensions to textContent type
 
     var supportedTypes = {
 
@@ -73,74 +63,33 @@ function servePage(request, response) {
 
     };
 
-    // Extract the filename and extension from the request.
-
     var filename = url.parse(request.url).pathname.substring(1);
-
-    // If the user does not enter a file name,
-
-    // we serve the page index.html
-
     if (!filename) {
-
         filename = 'index.html';
-
     }
-
-    // Get the extension of the requested resource so we can determine the type
-
     var extension = filename.substring( filename.lastIndexOf(".") + 1) ;
-
-    var type = supportedTypes[extension];  // type implied by extension
-
-    // Read the file asynchronously
-
+    var type = supportedTypes[extension];
     fs.readFile( filename, function( err, content) {
-
-        if (err) { // If there is an error, set the status code
-
-            response.writeHead( 404,
-
-                {'Content-Type': 'text/plain; charset = UTF-8'});
-
-            response.write( err.message); // Include the error message body
-
+        if (err) {
+            response.writeHead( 404, {
+                'Content-Type': 'text/plain; charset = UTF-8'
+            });
+            response.write( err.message);
             response.write( ' - The page requested is not found.');
-
-            response.end(); // Done
-
-        } else { // Otherwise, the file was read successfully.
-
-            response.writeHead( 200, // Set the status code
-
-                {'Content-Type': type});
-
-            response.write(content); // Send file contents as response body
-
             response.end();
-
+        } else {
+            response.writeHead( 200, {
+                'Content-Type': type
+            });
+            response.write(content);
+            response.end();
         }
-
     });
-
 }
 
-// load the url module
-
 var url = require('url');
-
-// Load the file system module
-
 var fs = require("fs");
-
-// load the http module
-
 var http = require('http');
-
-// create a server object
-
 var server = http.createServer(servePage);
-
 server.listen(4040, 'localhost');
-
 console.log('Server running at http://localhost:4040');
